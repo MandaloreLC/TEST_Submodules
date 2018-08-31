5bfbc699a8c5c7f6a917e5337a6926704cd7cb8e
MandaloreLC <liviucucerenco@gmail.com>
2018-08-31 17:17:01 +0100

    Trial commit of change on submodule from root project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
+import testPackage.SubmoduleObject;
+
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Running root project" );
+        SubmoduleObject smo = new SubmoduleObject();
+        smo.printTwice( "Test" );
     }
 }
